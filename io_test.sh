fc69562627105654d5d5b6093c943190033f3634
Sergey Utkin <utkins01@gmail.com>
2020-12-08 06:41:02 +0300

    add lead time task

--- io_test.sh
+++ io_test.sh
@@ -18,7 +18,7 @@ echo ""
 echo "Zero test speed: $(dd if=/dev/zero of=/dev/null bs=1M count=1024 2>&1 | tail -n1 | awk '{print $(NF-1), $NF}')"
 echo ""
 
-printf '%-20s%12s%12s\n' "TEST" "WRITE" "READ"
+printf '%-20s%12s%10s%12s%10s\n' "TEST" "WRITE" "SEC" "READ" "SEC"
 
 for test in ${TESTS}; do 
     if [ $(id -u) = 0 ]; then
@@ -29,9 +29,11 @@ for test in ${TESTS}; do
     TEST_FILE="$TEST_FOLDER/$BS-$COUNT.iotest"
     BS=$(echo $test | cut -d ":" -f 1)
     COUNT=$(echo $test | cut -d ":" -f 2)
-    WRITE=$(dd if=/dev/zero of=$TEST_FILE bs=$BS count=$COUNT 2>&1 | tail -n1 | awk '{print $(NF-1), $NF}')
-    READ=$(dd if=$TEST_FILE of=/dev/null bs=$BS count=$COUNT 2>&1 | tail -n1 | awk '{print $(NF-1), $NF}')
-    printf '%-20s%12s%12s\n' "bs=$BS count=$COUNT" "$WRITE" "$READ"
+    WRITE=$(dd if=/dev/zero of=$TEST_FILE bs=$BS count=$COUNT 2>&1 | tail -n1 | awk '{print $(NF-1), $NF, $(NF-3)}')
+    WRITE=($WRITE)
+    READ=$(dd if=$TEST_FILE of=/dev/null bs=$BS count=$COUNT 2>&1 | tail -n1 | awk '{print $(NF-1), $NF, $(NF-3)}')
+    READ=($READ)
+    printf '%-20s%12s%10.3f%12s%10.3f\n' "bs=$BS count=$COUNT" "${WRITE[0]} ${WRITE[1]}" "${WRITE[2]}" "${READ[0]} ${READ[1]}" "${READ[2]}"
     rm $TEST_FILE
 done
 
